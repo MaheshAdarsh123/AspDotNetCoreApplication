075dfe2c3288b9bd02c4325d2189e24da4f17f4a
MaheshAdarsh <MaheshAdarsh@DESKTOP-A72N2LV>
2022-06-30 07:44:40 +0530

    Added MVC to the application

---
+++ BookApp/Controllers/HomeControllercs.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookApp.Controllers
+{
+    public class HomeControllercs : Controller
+    {
+        public string Index()
+        {
+            return "HomeController action menthod";
+        }
+    }
+}
--- BookApp/Startup.cs
+++ BookApp/Startup.cs
@@ -16,24 +16,48 @@ namespace BookApp
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            // In this method we can also add the HTTP pipeline
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
+            //app.Use(async (Context, next) =>
+            //{
+            //    await Context.Response.WriteAsync("Hello from 1st MiddleWare");
+            //    await next();
+            //    await Context.Response.WriteAsync("Hello from 1st MiddleWare");
+            //});
+
+            //app.Use(async (Context, next) =>
+            //{
+            //    await Context.Response.WriteAsync("Hello from 2nd MiddleWare");
+            //    await next();
+            //    await Context.Response.WriteAsync("Hello from 2nd MiddleWare");
+            //});
+
+            //app.Use(async (Context, next) =>
+            //{
+            //    await Context.Response.WriteAsync("Hello from 2nd MiddleWare");
+            //    await next();
+            //});
+
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                //endpoints.MapGet("/", async context =>
+                //{
+                //    await context.Response.WriteAsync("Hello World!");
+                //});
+                endpoints.MapDefaultControllerRoute();
             });
         }
     }
